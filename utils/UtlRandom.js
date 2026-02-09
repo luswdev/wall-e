aadb759d59f2747be12b3b59614b04c8ab9c2b19
LuSkywalker <callumlu@gmail.com>
2026-02-09 18:45:10 +0800

    [A] add random utility

---
+++ utils/UtlRandom.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const MersenneTwister = require('mersenne-twister')
+
+class Random {
+    constructor () {
+        this.twister = new MersenneTwister()
+    }
+
+    initRandom () {
+        this.twister.init_seed(Date.now())
+    }
+
+    getRandomRange (_range) {
+        return Math.floor(this.twister.random() * Math.floor(_range))
+    }
+}
+
+const randomInst = new Random()
+
+module.exports.random = randomInst
